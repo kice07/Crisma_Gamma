16601aed462c9c3c73802cc40262a4f54427cd9e
kice07 <christ.ehouman07@gmail.com>
2024-07-16 20:56:46 -0400

    no-contrat
    Signed-off-by: kice07 <christ.ehouman07@gmail.com>

--- Freelancer_Dashboard/index.php
+++ Freelancer_Dashboard/index.php
@@ -30,8 +30,8 @@ $_SESSION['availability'] = $freelancer_row['availability'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="freelancer_dashboard_assets/css/navbar.css">
-    <!-- <link rel="stylesheet" href="freelancer_dashboard_assets/css/Freelancer_dashboard.css"> -->
-    <link rel="stylesheet" href="freelancer_dashboard_assets/css/Index.css">
+    <link rel="stylesheet" href="freelancer_dashboard_assets/css/Freelancer_dashboard.css">
+    <!-- <link rel="stylesheet" href="freelancer_dashboard_assets/css/Index.css"> -->
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css'>
