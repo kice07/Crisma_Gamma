6cda5e5a0f6994664cf0d98921e8c2f1174e9790
kice07 <christ.ehouman07@gmail.com>
2024-08-03 15:38:45 -0400

    composer
    Signed-off-by: kice07 <christ.ehouman07@gmail.com>

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__,'config.env');
 $dotenv->load();
 
 // Accéder aux variables d'environnement
