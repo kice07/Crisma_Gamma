cbb86e7047c38f1c45d0c289a46d37506e383313
kice07 <christ.ehouman07@gmail.com>
2024-08-03 13:01:17 -0400

    config
    Signed-off-by: kice07 <christ.ehouman07@gmail.com>

--- config.php
+++ config.php
@@ -6,4 +6,30 @@ if (!$conn) {
     echo "Echec de la connexion";
 }
  
+
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Accéder aux variables d'environnement
+$dbConnection = $_ENV['DB_CONNECTION'];
+$dbHost = $_ENV['DB_HOST'];
+$dbPort = $_ENV['DB_PORT'];
+$dbDatabase = $_ENV['DB_DATABASE'];
+$dbUsername = $_ENV['DB_USERNAME'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+
+// Connexion à la base de données
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
+
+if (!$conn) {
+    die("Connection failed: ");
+}
+echo "Connected successfully";
+
 ?>
